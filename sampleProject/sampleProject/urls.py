6f8e46ced7d16d3ccee45e1b90651d9d62a12c69
VIGNESHWARAN-USER <vigneshwaran.coder@gmail.com>
2025-03-11 05:26:38 +0530

    Removed templates in settings

--- sampleProject/sampleProject/urls.py
+++ sampleProject/sampleProject/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, re_path
 from django.views.generic import TemplateView
 urlpatterns = [
     path('', include('jsw.urls')),
-    re_path(r'^.*$', include('jsw.urls')),
     path('admin/', admin.site.urls),
 ]
